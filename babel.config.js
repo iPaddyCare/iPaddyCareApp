3177ad499ebf59ca37e414563f3b0002ccf0bf47
Kavindu Santhusha <ksanthusha20@gmail.com>
2025-06-21 22:50:59 +0530

    Fix Metro bundler error

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
+console.log('Loading Babel config from iPaddyCare root');
 module.exports = {
-  presets: ["module"],
-  plugins: ["react-native-reanimated/plugin"],
+  presets: ['@react-native/babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
 };
