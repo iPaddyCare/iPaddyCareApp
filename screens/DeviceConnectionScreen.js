e5ea216299adc3893613305122985db08251e227
Kavindu Santhusha <ksanthusha20@gmail.com>
2026-03-10 08:09:12 +0530

    Revert "feat: Implement or update device connection logic and UI in DeviceConnectionScreen."
    This reverts commit dedc7fd9109990c74865da2c023fba50e05c41c2.

--- screens/DeviceConnectionScreen.js
+++ screens/DeviceConnectionScreen.js
@@ -9,7 +9,6 @@ import {
   ActivityIndicator,
   Alert,
   TextInput,
-  Dimensions,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ESP32Service from '../src/utils/esp32Service';
